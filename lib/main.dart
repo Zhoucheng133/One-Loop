aa4610f0bd50814b70cc5242b97b112635e5667e
zhoucheng <zhoucheng133@outlook.com>
2026-01-14 23:57:33 +0800

    添加繁体中文支持

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ import 'package:one_loop/controllers/audio.dart';
 import 'package:one_loop/controllers/controller.dart';
 import 'package:one_loop/lang/en_us.dart';
 import 'package:one_loop/lang/zh_cn.dart';
+import 'package:one_loop/lang/zh_tw.dart';
 import 'package:one_loop/main_view.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
@@ -16,7 +17,7 @@ void main() {
   ]);
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
   final controller=Get.put(Controller());
-  controller.init();
+  await controller.init();
   runApp(const MainApp());
 }
 
@@ -25,6 +26,7 @@ class MainTranslations extends Translations {
   Map<String, Map<String, String>> get keys => {
     'en_US': enUS,
     'zh_CN': zhCN,
+    'zh_TW': zhTW,
   };
 }
 
@@ -43,37 +45,39 @@ class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     final brightness = MediaQuery.of(context).platformBrightness;
-    return GetMaterialApp(
-      translations: MainTranslations(),
-      locale: Get.deviceLocale,
-      fallbackLocale: Locale('en', 'US'),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate
-      ],
-      supportedLocales: [
-        Locale('en', 'US'),
-        Locale('zh', 'CN'),
-        Locale('zh', 'TW'),
-      ],
-      theme: ThemeData(
-        brightness: brightness,
-        fontFamily: 'PuHui', 
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.pinkAccent,
+    return Obx(
+      () => GetMaterialApp(
+        translations: MainTranslations(),
+        locale: controller.lang.value.locale,
+        fallbackLocale: Locale('en', 'US'),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate
+        ],
+        supportedLocales: [
+          Locale('en', 'US'),
+          Locale('zh', 'CN'),
+          Locale('zh', 'TW'),
+        ],
+        theme: ThemeData(
           brightness: brightness,
+          fontFamily: 'PuHui', 
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.pinkAccent,
+            brightness: brightness,
+          ),
+          textTheme: brightness==Brightness.dark ? ThemeData.dark().textTheme.apply(
+            fontFamily: 'PuHui',
+            bodyColor: Colors.white,
+            displayColor: Colors.white,
+          ) : ThemeData.light().textTheme.apply(
+            fontFamily: 'PuHui',
+          ),
         ),
-        textTheme: brightness==Brightness.dark ? ThemeData.dark().textTheme.apply(
-          fontFamily: 'PuHui',
-          bodyColor: Colors.white,
-          displayColor: Colors.white,
-        ) : ThemeData.light().textTheme.apply(
-          fontFamily: 'PuHui',
-        ),
+        debugShowCheckedModeBanner: false,
+        home: MainView()
       ),
-      debugShowCheckedModeBanner: false,
-      home: MainView()
     );
   }
 }
